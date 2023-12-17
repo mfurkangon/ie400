cb4740b306748017a15ba5b9dd550e6c22748ddf
O-Hamdash <ohamdash@hotmail.com>
2023-12-17 12:52:06 +0300

    added documentation

--- q1.py
+++ q1.py
@@ -1,8 +1,10 @@
 import cplex
 
+# values of the file depot_node_distances.txt
 x_to_stations = [1, 1, 1, 2, 3, 2, 1, 1]
 y_to_stations = [3, 2, 1, 1, 1, 1, 1, 2]
 
+# values of the file distances.txt
 distances = [
                 [0, 1, 2, 1, 1, 2, 2, 1], 
                 [1, 0, 1, 1, 2, 3, 1, 3], 
@@ -14,6 +16,7 @@ distances = [
                 [1, 3, 2, 2, 2, 2, 1, 0]
             ]
 
+# paths from the file paths.txt
 paths = [
             ['A', 'C', 'H', 'B'],
             ['B', 'G', 'A'],
@@ -32,9 +35,19 @@ paths = [
             ['B', 'D', 'G']
         ]
 
-stations = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+# This dictionary helps convert the station name into its index
+# Usage example: distance from station A to station F = distances[station['A'], station['F']]
+station = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def calculate_path_length(path_index):
+    """ 
+    Args:
+        - path_index: index of the path from the paths array
+
+    Returns:
+        - length of the path, example: for A-C-F it will return (distance A to C) + (distance C to F)
+    """
+
     path = paths[path_index]
     num_stations = len(path)
 
@@ -44,11 +57,21 @@ def calculate_path_length(path_index):
         start = path[i]
         end = path[i + 1]
         
-        path_length += distances[stations[start]][stations[end]]
+        path_length += distances[station[start]][station[end]]
 
     return path_length
 
 def calculate_depot_distance(path_index, is_x):
+    """ 
+    Args:
+        - path_index: index of the path from the paths array
+        - is_x (Bool): x is used as the depot, if false it will consider that Y is the depot
+
+    Returns:
+        - The distance from the depot to the starting station of the path + the distance from the last station of the path to the depot
+        - Example: for A-B-C if is_x == True it will return distance X to A + distance C to X
+    """
+
     if is_x:
         dists = x_to_stations
     else:
@@ -58,19 +81,28 @@ def calculate_depot_distance(path_index, is_x):
     start = path[0]
     end = path[-1]
 
-    return dists[stations[start]] + dists[stations[end]]
+    return dists[station[start]] + dists[station[end]]
 
 def calculate_loop_distance(path_index):
+    """ 
+    Args:
+        - path_index: index of the path from the paths array
+
+    Returns:
+        - Distance between start station and end station of the path
+    """
+
+
     path = paths[path_index]
     start = path[0]
     end = path[-1]
 
-    return distances[stations[start]][stations[end]]
+    return distances[station[start]][station[end]]
 
 
-path_lengths = [0] * 15
-depot_distances = [[0] * 15, [0] * 15]
-loop_distances = [0] * 15
+path_lengths = [0] * 15 # path lengths for all 15 paths
+depot_distances = [[0] * 15, [0] * 15] # depot distances for all 15 paths (first sub array is for depot X and the second is for depot Y)
+loop_distances = [0] * 15 # loop distances for all 15 paths
 
 for i in range(15):
     is_x = True
@@ -80,7 +112,7 @@ for i in range(15):
         is_x = False
     loop_distances[i] = calculate_loop_distance(i)
 
-num_of_loops = [[0] * 15, [0] * 15]
+num_of_loops = [[0] * 15, [0] * 15] # The number of times we can loop through one path considering the restriction of 4h service per day (20h operation per day)
 prev = 1
 curr = 1
 for i in range(15):
@@ -94,19 +126,16 @@ for i in range(15):
 # Create a CPLEX problem
 problem = cplex.Cplex()
 
+# String names of the variables
 x_names = [f"x{i+1}" for i in range(15)]
-z_names = [f"z{i+1}" for i in range(15)]
 
 # Add binary variables x
 problem.variables.add(names=x_names, lb=[0]*len(x_names), ub=[1]*len(x_names), types=["B"]*len(x_names))
 
-# Add integer variables z
-problem.variables.add(names=z_names, lb=[0]*len(z_names), ub=[cplex.infinity]*len(z_names), types=["I"]*len(z_names))
-
-
+# Set objective function type
 problem.objective.set_sense(problem.objective.sense.minimize)
 
-objective_function = {}
+objective_function = {} # This is used to hold objective the function's elements
 for i in range(15):
     # Summing up the coefficients for each x_names[i]
     if x_names[i] not in objective_function:
@@ -114,6 +143,8 @@ for i in range(15):
     objective_function[x_names[i]] += depot_distances[0][i] - depot_distances[1][i]
 
 # Add the offset
+# Used to add additional constant addition or substractions
+# For numbers not multiplied by any decision variables
 problem.objective.set_offset(sum(depot_distances[1]))
 
 # set the linear part of the objective function
@@ -124,6 +155,7 @@ problem.objective.set_linear(objective_function.items())
 # for simplicity
 b = depot_distances
 
+# Loop to add all the constraints
 for i in range(15):
 
     rhs = 20
@@ -136,6 +168,8 @@ for i in range(15):
         rhs=[rhs]
     )
 
+
+# Global constraints that do not requre looping can be added outside the loop like this
 problem.linear_constraints.add(
     lin_expr=[[x_names, [1] * len(x_names)]],
     senses=["L"],
@@ -167,6 +201,7 @@ try:
     for name in x_names:
         print(f"{name}: {solution.get_values(name)}")
 
+    # Print the objective value
     print("Objective value:", solution.get_objective_value())
 
 except cplex.CplexError as e:
